63508cb90597af1f7b0d117ebd5152b6ed0d1269
abadfish <kirsten.ofarrell@gmail.com>
2016-09-17 23:40:44 -0400

    spacing on about page and posts show

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  
+  # Had to add these 3 lines to make pictures show up on heroku
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
