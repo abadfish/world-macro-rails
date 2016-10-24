59a55f45ca43e1e8e56f1d8f66e0625b48297a19
abadfish <kirsten.ofarrell@gmail.com>
2016-10-23 22:49:49 -0400

    added images to s3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def s3_image_path(filename)
+  "http://s3.amazonaws.com/world-macro-charts/#{filename}"
+  end
+
+  def s3_image_tag(filename, options={})
+    image_tag(s3_image_path(filename), options)
+  end
+
 end
